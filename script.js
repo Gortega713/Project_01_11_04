caae19968c2b44628d1c87b896be8bad0860ba25
Unknown <Gortega713@west-mec.org>
2018-09-04 18:26:24 -0700

    Started code

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 /*  Project 01_11_04
 
-    Author: 
-    Date:   
+    Author: Gabriel Ortega
+    Date: 9.4.18  
 
     Filename: script.js
 */
 
 "use strict";
 
+// Global Variables
+
+var httpRequest = false;
+
+
+
+// Event Listeners
+var zip = document.getElementById("zip");
+if (zip.addEventListener) {
+    zip.addEventListener("keyup", checkInput, false)
+} else if (zip.attachEvent){
+    zip.attachEvent("onkeyup", checkInput);
+}
+
+function checkInput() {
+    alert("checkInput()");
+}
