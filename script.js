42ea2a2a6ef910458d5a35ce07c98b9dc5cf5c7f
Unknown <Gortega713@west-mec.org>
2018-09-05 18:26:24 -0700

    Continued code
    Added two functions. Added getLocation() and getRequestObject().

--- script.js
+++ script.js
@@ -12,16 +12,50 @@
 
 var httpRequest = false;
 
-
+// Function to create an XHR object
+function getRequestObject() {
+    try {
+        httpRequest = new XMLHttpRequest();
+    } catch (requestError) {
+        return false;
+    }
+    document.getElementById("csset").style.visibility = "visible";
+    var zip = document.getElementById("zip");
+    if (zip.addEventListener) {
+        zip.removeEventListener("keyup", checkInput, false)
+    } else if (zip.attachEvent) {
+        zip.detachEvent("onkeyup", checkInput);
+        return httpRequest;
+    }
+}
 
 // Event Listeners
 var zip = document.getElementById("zip");
 if (zip.addEventListener) {
     zip.addEventListener("keyup", checkInput, false)
-} else if (zip.attachEvent){
+} else if (zip.attachEvent) {
     zip.attachEvent("onkeyup", checkInput);
 }
 
+// Function to check if the value is a vlaid 5-digit zip code, if so, run getLocation()
 function checkInput() {
-    alert("checkInput()");
+    var zip = document.getElementById("zip").value;
+    if (zip.length === 5) {
+        getLocation()
+    } else {
+        document.getElementById("city").value = "";
+        document.getElementById("state").value = "";
+    }
+}
+
+// Function to create a new XHR object or reuse one
+function getLocation() {
+    var zip = document.getElementById("zip").value;
+    if (!httpRequest) {
+        httpRequest = getRequestObject();
+    } else {
+        httpRequest.abort();
+        httpRequest.open("get", "http://api.zippopotam.us/us/" + zip, true);
+        httpRequest.send(null);
+    }
 }
